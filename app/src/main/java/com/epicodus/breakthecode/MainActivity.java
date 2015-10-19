3a16f7830f13ea46e757e8f1d06f5c93b0baae25
Aimee <oem@oems-MacBook-Pro.local>
2015-10-19 16:47:45 -0700

    Initial commit

---
+++ app/src/main/java/com/epicodus/breakthecode/MainActivity.java
@@ -0,0 +1,49 @@
+package com.epicodus.breakthecode;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mEnrollButton;
+    private Button mSponserButton;
+    private Button mNonProfitPartnersButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEnrollButton = (Button) findViewById(R.id.EnrollButton);
+        mSponserButton = (Button) findViewById(R.id.SponserButton);
+        mNonProfitPartnersButton = (Button) findViewById(R.id.NonProfitButton);
+
+        mNonProfitPartnersButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NonProfitActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mEnrollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, EnrollActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mSponserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SponserActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
